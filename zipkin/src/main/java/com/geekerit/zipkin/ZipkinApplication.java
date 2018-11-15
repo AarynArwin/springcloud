0645e762a5572481285eac32f54c906913c595b5
AarynArwin <m18234103595@163.com>
2018-11-15 17:48:07 +0800

    服务链路追踪并保存

--- zipkin/src/main/java/com/geekerit/zipkin/ZipkinApplication.java
+++ zipkin/src/main/java/com/geekerit/zipkin/ZipkinApplication.java
@@ -2,7 +2,13 @@ package com.geekerit.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import zipkin.server.EnableZipkinServer;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
 
 /**
  * @ClassName ZipkinApplication
@@ -12,10 +18,17 @@ import zipkin.server.EnableZipkinServer;
  * @Version 1.0
  */
 @SpringBootApplication
-@EnableZipkinServer
+//@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZipkinApplication.class,args);
+    }
+
+    @Bean
+    @Primary
+    public MySQLStorage mySQLStorage(DataSource datasource) {
+        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
     }
 }
